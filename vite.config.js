eeba310072e727f8070d2b04147164e165d0501c
xinxin <2316593341@qq.com>
2024-08-08 15:33:00 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from '@vant/auto-import-resolver';
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     AutoImport({
