93f543cf80331dae6e4715c39065585b7b0509b7
xinxin <2316593341@qq.com>
2024-08-09 08:56:05 +0800

    deploy_test

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import { vueRouters } from '../service/global'
+import Home from '@/views/home/home.vue'
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
@@ -9,38 +10,39 @@ const router = createRouter({
     },
     {
       path: '/home',
-      component: () => import('@/views/home/home.vue')
+      // component: () => import('@/views/home/home.vue')
+      component: Home
     },
-    // {
-    //   path: '/favor',
-    //   component: () => import('@/views/favor/favor.vue')
-    // },
-    // {
-    //   path: '/order',
-    //   component: () => import('@/views/order/order.vue')
-    // },
-    // {
-    //   path: '/message',
-    //   component: () => import('@/views/message/message.vue'),
-    //   meta:{
-    //     hiddenBar: true
-    //   }
-    // },
-    // {
-    //   path: '/city',
-    //   component: () => import('@/views/city/city.vue'),
-    //   meta:{
-    //     hiddenBar: true
-    //   }
-    // },
-    // {
-    //   path: '/detail/:id',
-    //   component: () => import('@/views/detail/detail.vue'),
-    //   meta: {
-    //     hiddenBar: true
-    //   }
-    // }
-    ...vueRouters()
+    {
+      path: '/favor',
+      component: () => import('@/views/favor/favor.vue')
+    },
+    {
+      path: '/order',
+      component: () => import('@/views/order/order.vue')
+    },
+    {
+      path: '/message',
+      component: () => import('@/views/message/message.vue'),
+      meta:{
+        hiddenBar: true
+      }
+    },
+    {
+      path: '/city',
+      component: () => import('@/views/city/city.vue'),
+      meta:{
+        hiddenBar: true
+      }
+    },
+    {
+      path: '/detail/:id',
+      component: () => import('@/views/detail/detail.vue'),
+      meta: {
+        hiddenBar: true
+      }
+    }
+    // ...vueRouters()
   ]
 })
 
